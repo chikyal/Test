270be6b1769a1c119b263fe2d150b4750e527ed4
Stephen Kilsby <steve@buildsoft.com.au>
2017-03-27 13:01:21 +1100

    Initial project uploaded

---
+++ QuizApi/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using QuizApi.Utils;
+
+namespace QuizApi.Controllers
+{
+    [RoutePrefix("demo")]
+    public class DemoController : BaseController
+    {
+        [Route("time")]
+        public HttpResponseMessage GetCurrentAccount(int version)
+        {
+            ValidateApiVersionAndState(version);
+            
+            return Request.CreateResponse(HttpStatusCode.OK, new { ServerTime = DateTime.UtcNow });
+        }
+
+      
+    }
+}
---
+++ WebQuiz/app/components/default.ts
@@ -0,0 +1,30 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { DemoService } from "../services/demo"
+
+@Component({
+    selector: "default-component",
+    templateUrl: "/partial/default",
+    providers: [DemoService]
+})
+
+export class DefaultComponent implements OnInit {
+    serverTime: Date;
+
+    constructor(private demoService: DemoService) { }
+
+    getTimeFromServer(): void {
+        this.demoService.getTime().then((result) => {
+            this.serverTime = result.ServerTime;
+        });
+    }
+
+    ngOnInit(): void {
+        this.getTimeFromServer();
+
+        this.demoService.notifyBuildSuccess().then(() => {
+            console.info("Build notified");
+        }).catch(() => {
+            console.error("Build notification error");
+        });
+    }
+}
